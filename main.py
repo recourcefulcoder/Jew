98f20e0895d2ce53c7482f17e82268b4aa39a796
kiric <kirichenko.grigorij@inbox.ru>
2021-12-09 18:25:36 +0300

    Changed ui-initialization + random colors system added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
 from random import randint
 
 
@@ -9,15 +11,43 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-class Application(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(542, 519)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.flex_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.flex_btn.setGeometry(QtCore.QRect(190, 430, 151, 31))
+        self.flex_btn.setObjectName("flex_btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 542, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.flex_btn.setText(_translate("MainWindow", "Устроить флекс"))
+
+
+class Application(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
+        self.retranslateUi(self)
         self.can_paint = False
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("Шарики!!")
+        # self.setWindowTitle("Шарики!!")
         self.flex_btn.clicked.connect(self.paint_circles)
 
     def paint_circles(self):
@@ -28,11 +58,12 @@ class Application(QMainWindow):
         qp.setBrush(QColor(255, 255, 255))
         qp.setPen(QColor(255, 255, 255))
         qp.drawRect(0, 0, 541, 411)
-        qp.setBrush(QColor(255, 255, 0))
-        qp.setPen(QColor(255, 255, 0))
         drawed_circles = list()
         value = randint(5, 50)
         for i in range(value):
+            color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+            qp.setBrush(color)
+            qp.setPen(color)
             radius = randint(1, 50)
             x_coord = randint(0, 541 - radius)
             y_coord = randint(0, 411 - radius)
